d84fe8ff3c8e954bcd843374cc7bd2c7f9513773
GRXcoding <gzlbernal@gmail.com>
2025-11-05 12:55:09 +0100

    Añadir credenciales de FB

--- Maquetación Básica CSS y Flexbox/firebase-config.js
+++ Maquetación Básica CSS y Flexbox/firebase-config.js
@@ -4,14 +4,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-// TODO: REEMPLAZA ESTO CON LA CONFIGURACIÓN DE TU PROYECTO
 const firebaseConfig = {
-  apiKey: "AIzaSy...TU_API_KEY",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyB11fwCT25q8QAmmVrtQYVk2DBycLCM--g",
+  authDomain: "maquetacion-gzbl.firebaseapp.com",
+  projectId: "maquetacion-gzbl",
+  storageBucket: "maquetacion-gzbl.firebasestorage.app",
+  messagingSenderId: "384557777640",
+  appId: "1:384557777640:web:7264eee159d22c5082ff5d"
 };
 
 // Inicializar Firebase
